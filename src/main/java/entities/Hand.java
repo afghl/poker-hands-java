eb3a70c63a71398e330fa475abd5d6f81d46224d
afghl <airport.0930@gmail.com>
2016-11-22 10:52:26 +0800

    get rid of valueset

--- src/main/java/entities/Hand.java
+++ src/main/java/entities/Hand.java
@@ -5,17 +5,11 @@ import java.util.*;
 public class Hand {
     private final static String VALUE_ORDER = "23456789TJQKA";
 
-    private final static int CARDS_NUMBER = 5;
-
-    private int[] values = new int[CARDS_NUMBER];
+    private List<Integer> values = new ArrayList<Integer>();
 
     private Set<Character> types = new HashSet<Character>();
 
     private List<Card> cards = new ArrayList<Card>();
-
-    private int count = 0;
-
-    private int highest = -1;
 
     private Hand() {}
 
@@ -25,130 +19,55 @@ public class Hand {
         for (String c : cards)
             h.addCard(c);
 
-        Arrays.sort(h.values);
+        Collections.sort(h.values);
+
         return h;
     }
 
     public String pattern() {
+        SortedSet<Integer> valueSet = new TreeSet<Integer>(values);
+
         if (types.size() == 1 && consecutiveValues()) {
             return "Straight Flush";
-        } else if (fourOfKind()) {
+        } else if (valueSet.size() == 2 &&
+                Collections.frequency(values, valueSet.first()) == 1 ||
+                Collections.frequency(values, valueSet.first()) == 4
+                ) {
             return "Four of a Kind";
-        } else if (fullHouse()) {
+        } else if (valueSet.size() == 2 &&
+                Collections.frequency(values, valueSet.first()) == 2 ||
+                Collections.frequency(values, valueSet.first()) == 3
+                ) {
             return "Full House";
-        } else if (types.size() == 1 && !consecutiveValues()) {
-            return "Straight";
-        } else if (threeOfKind()) {
+        } else if (types.size() == 1) {
+            return "Flush";
+        } else if (threeOfKind(valueSet)) {
             return "Three of a Kind";
-        } else if (twoPairs()) {
-            return "Two Pairs";
-        } else if (pair()) {
-            return "Pair";
         } else {
-            return "No Pattern";
+            return detectPairs(valueSet);
         }
     }
 
-    private boolean fullHouse() {
-        int count;
-        boolean canBeFullHouse = false;
-
-        for (int i = 0; i < values.length; i++) {
-            count = 0;
-            for (int j = 0; j < values.length; j ++) {
-                if (values[i] == values[j]) count++;
-            }
-
-            // TODO: this is a bug here.
-            if (count == 3 && !canBeFullHouse) {
-                highest = values[i];
-                canBeFullHouse = true;
-            } else if (count == 2 && canBeFullHouse) {
-                return true;
-            } else if (count == 2 && !canBeFullHouse) {
-                canBeFullHouse = true;
-            } else if (count == 3 && canBeFullHouse) {
-                highest = values[i];
-                return true;
-            }
-        }
-        return false;
+    private String detectPairs(SortedSet valueSet) {
+        if (valueSet.size() == 3)
+            return "Two Pairs";
+        else if (values.size() == 4)
+            return "Pairs";
+        else
+            return "High Card";
     }
 
-    private boolean pair() {
-        int count;
-
-        for (int i = 0; i < values.length; i++) {
-            count = 0;
-            for (int j = 0; j < values.length; j ++) {
-                if (values[i] == values[j]) count++;
-            }
-
-            if (count == 2) {
-                highest = values[i];
+    private boolean threeOfKind(SortedSet<Integer> valueSet) {
+        for (int v : valueSet) {
+            if (Collections.frequency(values, v) > 0)
                 return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean twoPairs() {
-        int count;
-        boolean canBeTwoPair = false;
-
-        for (int i = 0; i < values.length; i++) {
-            count = 0;
-            for (int j = 0; j < values.length; j ++) {
-                if (values[i] == values[j]) count++;
-            }
-
-            if (count == 2 && !canBeTwoPair) {
-                highest = values[i];
-                canBeTwoPair = true;
-            } else if (count == 2 && canBeTwoPair) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean threeOfKind() {
-        int count;
-
-        for (int i = 0; i < values.length; i++) {
-            count = 0;
-            for (int j = 0; j < values.length; j ++) {
-                if (values[i] == values[j]) count++;
-            }
-
-            if (count == 3) {
-                highest = values[i];
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean fourOfKind() {
-        int count;
-
-        for (int i = 0; i < values.length; i++) {
-            count = 0;
-            for (int j = 0; j < values.length; j ++) {
-                if (values[i] == values[j]) count++;
-            }
-
-            if (count == 4) {
-                highest = values[i];
-                return true;
-            }
         }
         return false;
     }
 
     private boolean consecutiveValues() {
-        for (int i = 1; i < values.length; i++) {
-            if (values[i] - values[i - 1] != 1)
+        for (int i = 1; i < values.size(); i++) {
+            if (values.get(i) - values.get(i - 1) != 1)
                 return false;
         }
         return true;
@@ -159,7 +78,7 @@ public class Hand {
         int value = VALUE_ORDER.indexOf(c.charAt(0));
 
         types.add(type);
-        values[count++] = value;
-        cards.add(new Card(value, type));
+        values.add(value);
+//        cards.add(new Card(value, type));
     }
 }
