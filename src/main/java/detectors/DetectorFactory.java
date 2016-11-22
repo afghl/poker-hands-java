bf863a97028c512ab62b6f7204fb7a83cf196202
afghl <airport.0930@gmail.com>
2016-11-22 11:42:51 +0800

    use factory

---
+++ src/main/java/detectors/DetectorFactory.java
@@ -0,0 +1,11 @@
+package detectors;
+
+import entities.Hand;
+
+public abstract class DetectorFactory {
+    private DetectorFactory() {}
+
+    public static Detector<Hand> create() {
+        return new StraightFlushDetector();
+    }
+}
--- src/test/java/detectors/DetectorTest.java
+++ src/test/java/detectors/DetectorTest.java
@@ -10,7 +10,7 @@ public class DetectorTest {
     public void detector_can_detect_straight_flush() {
         Hand h1 = Hand.createHand("2H 4H 3H 6H 5H");
         Hand h2 = Hand.createHand("2H 3D 4S 5C 6D");
-        Detector<Hand> d = new StraightFlushDetector();
+        Detector<Hand> d = DetectorFactory.create();
         Hand winner = d.judge(h1, h2);
         Assert.assertEquals(winner, h1);
     }
@@ -19,7 +19,7 @@ public class DetectorTest {
     public void detector_can_detect_two_starght_flush() {
         Hand h1 = Hand.createHand("2H 4H 3H 6H 5H");
         Hand h2 = Hand.createHand("1H 4H 3H 5H 2H");
-        Detector<Hand> d = new StraightFlushDetector();
+        Detector<Hand> d = DetectorFactory.create();
         Hand winner = d.judge(h1, h2);
         Assert.assertEquals(winner, h1);
     }
@@ -28,7 +28,7 @@ public class DetectorTest {
     public void detector_can_detect_tie() {
         Hand h1 = Hand.createHand("2H 4H 3H 6H 5H");
         Hand h2 = Hand.createHand("6H 4H 3H 5H 2H");
-        Detector<Hand> d = new StraightFlushDetector();
+        Detector<Hand> d = DetectorFactory.create();
         Hand winner = d.judge(h1, h2);
         Assert.assertEquals(winner, null);
     }
